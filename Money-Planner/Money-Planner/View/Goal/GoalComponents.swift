7f9972fd1ec7a3c7b1e1e22eab5482ac40ce8586
Park-kxng <coding.parkgy29@gmail.com>
2024-05-17 15:24:00 +0900

    [FIX] 목표 - 목표 이름 설정 시 텍스트색 수정 (회색 -> 검정색)

--- Money-Planner/Money-Planner/View/Goal/GoalComponents.swift
+++ Money-Planner/Money-Planner/View/Goal/GoalComponents.swift
@@ -649,7 +649,7 @@ class WriteNameView: UIView {
         textField.translatesAutoresizingMaskIntoConstraints = false
         addSubview(textField)
         
-        textField.textColor = UIColor.mpGray
+        textField.textColor = UIColor.mpBlack
         textField.textAlignment = .left
         textField.placeholder = "목표 이름"
         
